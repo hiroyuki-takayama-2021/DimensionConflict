931e9b9dc89812efd76062c8f1f17bea88667f2d
Vonlet <31365674+Vonlet@users.noreply.github.com>
2021-01-31 16:26:48 +0900

    set

---
+++ src/main/java/com/github/chaosindustry/dimension_conflict/Dimension_conflict.java
@@ -0,0 +1,17 @@
+package com.github.chaosindustry.dimension_conflict;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dimension_conflict extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
